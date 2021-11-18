35c7cdac1f746694832cf22b986aa1bb6cf54c7d
jumpei-kurata <jumpei.kurata@rakus-partners.co.jp>
2021-11-18 10:31:09 +0900

    first commit

---
+++ src/main/java/com/example/GitAdvancedJohnApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitAdvancedJohnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitAdvancedJohnApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/domain/Car.java
@@ -0,0 +1,44 @@
+package com.example.domain;
+/**
+ * Carドメイン
+ * @author 81907
+ *
+ */
+public class Car {
+
+	private int speed;
+	private String name;
+
+//	引数ありコンストラクタ
+	public Car(int speed, String name) {
+		super();
+		this.speed = speed;
+		this.name = name;
+	}
+
+//	引数なしコンストラクタ
+	public Car() {
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Car [speed=" + speed + ", name=" + name + "]";
+	}
+
+}
